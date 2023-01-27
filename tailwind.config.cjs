56273013cbe4b1b03d78f70cbd90a2b761bd6d26
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-01-27 19:56:26 +0100

    change breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    screens: {
-      desktop: '376px'
-    },
     extend: {
       colors: {
         grayish: '#6b7189',
